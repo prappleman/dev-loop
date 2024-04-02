4af3228c9fffaecf07a6bee7db1d80716f43c3ee
parkerrappleye <parker.rappleye1@gmail.com>
2024-04-01 21:05:13 -0600

    added post model

--- models/Post.js
+++ models/Post.js
@@ -1,15 +1,46 @@
-const { Model, Sequelize, DataTypes } = require('sequelize');
+const {
+    Model,
+    DataTypes
+} = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {};
+
+class Post extends Model {}
 
 Post.init({
-        title: DataTypes.STRING,
-        body: DataTypes.STRING
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
     },
-    {
-        sequelize
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1]
+        }
+    },
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1]
+        }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
     }
-);
+}, {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+})
+
 
 module.exports = Post;
