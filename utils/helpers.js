3e1a7ffc927cf66cdeb0b2023fb251aad709aa98
parkerrappleye <parker.rappleye1@gmail.com>
2024-04-01 21:07:47 -0600

    utiles

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,10 @@
-// format_date fcn
+function formatDate(date) {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+        date
+      ).getFullYear()}`;
+}
+
 
 module.exports = {
-    format_date: date => {
-      return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-    }
-};
-  
+    formatDate
+}
