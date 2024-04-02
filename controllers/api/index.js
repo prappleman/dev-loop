98dc90f4e7ffed94a7fc15627561f0a1b16edf62
parkerrappleye <parker.rappleye1@gmail.com>
2024-04-01 21:06:57 -0600

    indexed the route js files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const postRoutes = require('./postRoutes');
-const commentRoutes = require('./commentRoutes');
 
-router.use('/user', userRoutes);
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes.js');
 
-router.use('/post', postRoutes);
-
-router.use('/comment', commentRoutes);
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes)
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,17 @@
 const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
-const homeRoutes = require('./homeRoutes.js');
-const dashboardRoutes = require('./dashboardRoutes.js');
-const apiRoutes = require('./api/index.js');
-
-router.use('/', homeRoutes);
-
-router.use('/dashboard', dashboardRoutes);
 
 router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/', homeRoutes);
+
+
+router.use((req, res) => {
+    res.status(404).end();
+});
+
 
 module.exports = router;
