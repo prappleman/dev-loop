fe68bd2509dc203e8fdc540060eec4985401f2f2
parkerrappleye <parker.rappleye1@gmail.com>
2024-04-02 22:02:09 -0600

    changed path for connections

--- models/Post.js
+++ models/Post.js
@@ -1,46 +1,16 @@
-const {
-    Model,
-    DataTypes
-} = require('sequelize');
-const sequelize = require('../config/connection');
-
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
 
 class Post extends Model {}
 
-Post.init({
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [1]
-        }
-    },
-    content: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [1]
-        }
-    },
-    user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'user',
-            key: 'id'
-        }
-    }
-}, {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'post'
-})
-
+Post.init(
+  {
+    title: DataTypes.STRING,
+    body: DataTypes.STRING
+  },
+  {
+    sequelize
+  }
+);
 
 module.exports = Post;
