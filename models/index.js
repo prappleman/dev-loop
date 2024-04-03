61d62434806be50cfc296f238423f801f4901b80
parkerrappleye <parker.rappleye1@gmail.com>
2024-04-02 22:01:47 -0600

    indexed models

--- models/index.js
+++ models/index.js
@@ -2,34 +2,23 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-
-User.hasMany(Post, {
-    foreignKey: 'user_id'
-})
-
-User.hasMany(Comment, {
-    foreignKey: 'user_id'
-})
-
 Post.belongsTo(User, {
-    foreignKey: 'user_id'
-})
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
 
 Post.hasMany(Comment, {
-    foreignKey: 'post_id'
-})
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
 
 Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-})
-
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-})
-
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
 
 module.exports = {
-    User,
-    Post,
-    Comment
+  User,
+  Comment,
+  Post
 };
